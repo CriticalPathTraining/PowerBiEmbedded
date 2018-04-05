7b658dd76b1436256527a74eda3c4a53e9b54b91
Ted Pattison <ted@tedpattison.net>
2018-04-05 12:40:26 -0400

    Updates

---
+++ Demos/PowerBiEmbeddedScratchpad/PowerBiEmbeddedScratchpad/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PowerBiEmbeddedScratchpad.Models;
+
+namespace PowerBiEmbeddedScratchpad {
+  class Program {
+    static void Main() {
+
+      //PageGenerator.GenerateReportPage();
+      // PageGenerator.GenerateReportPageFirstParty();
+      //PageGenerator.GenerateReportWithToolbarPage();
+      //PageGenerator.GenerateNewReportPage();
+      //PageGenerator.GenerateNewReportPageFirstParty();
+      //PageGenerator.GenerateDashboardPage();
+      //PageGenerator.GenerateDashboardTilePage();
+      //PageGenerator.GenerateReportInspectorPage();
+      PageGenerator.GenerateReportVisualPage();
+      //PageGenerator.GenerateQnaPage();
+    }
+
+  }
+}
